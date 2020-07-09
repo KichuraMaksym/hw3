ca6ee0620dec7eed19ff593cba93bde743cc1d31
kichuramaksym <https://github.com/KichuraMaksym>
2020-07-09 18:33:23 +0300

    1

--- 03/praktichna/README.md
+++ 03/praktichna/README.md
@@ -1,4 +1,4 @@
-# Practic
+# Praktichna
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.1.
 
--- 03/praktichna/e2e/src/app.e2e-spec.ts
+++ 03/praktichna/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('practic app is running!');
+    expect(page.getTitleText()).toEqual('praktichna app is running!');
   });
 
   afterEach(async () => {
