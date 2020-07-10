7b2bdd611366c510f26cc54bebe1cfe814e16fd2
kichuramaksym <https://github.com/KichuraMaksym>
2020-07-10 15:57:31 +0300

    2

---
+++ 04/hw1/src/app/servise/user.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {IUser} from '../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private Http: HttpClient) { }
+  getUsers(): Observable<IUser[]>{
+    return this.Http.get<IUser[]>(`https://jsonplaceholder.typicode.com/users`);
+  }
+}
